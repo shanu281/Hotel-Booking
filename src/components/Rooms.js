ee9d7d6dde2e4390cae9875d9b73ae9c037815ac
Shanu Kumar <shanukumar467@gmail.com>
2023-04-27 11:13:12 +0530

    Room card component & Rooms listing component updated

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -9,14 +9,17 @@ const Rooms = () => {
   return (
     <section className=' py-24'>
       <div className='container mx-auto lg:px-0'>
+        <div className='grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0'>
+
 {
   rooms.map((room) => {
-   
+    
     return (
       <Room key={room.id} room={room}/>
-    )
-  })
+      )
+    })
 }
+    </div>
       </div>
     </section>
   )
