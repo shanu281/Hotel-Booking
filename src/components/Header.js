2f8a2f4bcb86f4d38817517e726643a663b76491
Shanu Kumar <shanukumar467@gmail.com>
2023-04-28 20:07:40 +0530

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import LogoWhite from '../assets/img/logo-white.svg';
+import LogoDark from '../assets/img/logo-dark.svg';
+
 
 const Header = () => {
-  return <div>Header</div>;
+  const [header, setHeader] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false)
+    })
+  })
+  return (
+    <header className={`${header ? 'bg-white py-6 ' : 'bg-transparent py-8'} fixed z-50 w-full transition-all duration-300`}>
+      <a href='/'>
+{
+  header ? (<img className='w-[160px' src='{LogoDark}'/>) : (<img className='w-[160px' src='{LogoWhite}'/>)
+}
+      </a>
+
+    </header>
+  )
 };
 
 export default Header;
