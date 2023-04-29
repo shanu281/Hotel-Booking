557790bb51c52a175707ec804649e72347624037
Shanu Kumar <shanukumar467@gmail.com>
2023-04-29 10:33:09 +0530

    HeroSlider updated

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import { EffectFade, Autoplay } from "swiper";
+import Img1 from "../assets/img/heroSlider/1.jpg";
+import Img2 from "../assets/img/heroSlider/2.jpg";
+import Img3 from "../assets/img/heroSlider/3.jpg";
+import autoprefixer from "autoprefixer";
 
+const slides = [
+  {
+    title: "Your Luxury Hotel for Vaccation",
+    bg: Img1,
+    btnText: "SEE OUR ROOMS",
+  },
+  {
+    title: "Your Luxury Hotel for Vaccation",
+    bg: Img2,
+    btnText: "SEE OUR ROOMS",
+  },
+  {
+    title: "Your Luxury Hotel for Vaccation",
+    bg: Img3,
+    btnText: "SEE OUR ROOMS",
+  },
+];
 const HeroSlider = () => {
-  return <div className='heroslider h-[600px] lg:h-[860px] bg-black'>HeroSlider</div>;
+  return (
+    <Swiper 
+    modules={[EffectFade, Autoplay]}
+    effect={'fade'}
+    loop = {true}
+    autoplay={{delay:3000, disableOnInteraction:false, }}
+    className="heroslider h-[600px] lg:h-[860px]">
+      {slides.map((slide, index) => {
+        const { title, bg, btnText } = slide;
+        return (
+          <SwiperSlide
+            key={index}
+            className="h-full bg-pink-400 relative flex justify-center items-center"
+          >
+            <div className="z-20 text-white text-center">
+              <div className="uppercase font-tertiary tracking-[6px]">
+                Just enjoy and relax
+              </div>
+              <h1 className="text-[32px] font-primary uppercase tracking-[2px] max-w-[920px] lg:text-[68px] leading-tight mb-6">
+                {title}
+              </h1>
+              <button className="btn btn-lg btn-primary mx-auto">
+                {btnText}
+              </button>
+            </div>
+            <div className="absolute top-0 w-full h-full">
+              <img className="" src={bg} alt="image" />
+            </div>
+            <div className="absolute w-full h-full bg-black/70"></div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default HeroSlider;
