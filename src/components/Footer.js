ef5cbcad2059bf8ccdb73792ca3ae3c349323434
Shanu Kumar <shanukumar467@gmail.com>
2023-04-30 09:45:43 +0530

    heroSlider edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className=" bg-black px-4 py-6 ">
       <div className="text-white  mx-auto text-center">
-        Copyright &copy 2023. All rights reserved.
+        Copyright &copy; 2023. All rights reserved.
       </div>
     </footer>
   );
